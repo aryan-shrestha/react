f24ab8e0fbd313e1d66824b3d0a973f9dda77579
Aryan Shrestha <68954986+aryan-shrestha@users.noreply.github.com>
2022-03-27 20:47:50 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from "react";
+
+import React, { useState, useEffect, useRef } from "react";
+
+import Images from "./components/Images";
+import "./assets/css/style.css";
 
 function App() {
+  const [title, setTitle] = useState("hello React Hooks");
+  const [isShowing, setIsShowing] = useState(false);
+  const mountRef = useRef(false);
+
+  // component did mount only if empty array is passed
+  useEffect(() => {
+    console.log("app mounted");
+  }, []);
+
+  // useEffect for component did update
+  useEffect(() => {
+    if (mountRef.current) {
+      console.log("App updated");
+    } else {
+      mountRef.current = true;
+    }
+  }, [isShowing]);
+
+  function handleClick() {
+    setIsShowing(!isShowing);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="flex justify-center">
+      {console.log("app re-rendered")}
+      <div className="w-1/2">
+        <div className="text-center">
+          <div className="my-4">{title}</div>
+          <button
+            className="p-1 my-2 bg-blue-700 text-white"
+            onClick={handleClick}
+          >
+            Toggle Image
+          </button>
+        </div>
+
+        {isShowing ? <Images /> : null}
+      </div>
+    </section>
   );
 }
 
 export default App;
+
+// export default class App extends Component {
+//   constructor(props) {
+//     console.log("App Constructor");
+
+//     super(props);
+//     this.state = { title: "Hello React State", isShowing: false };
+//   }
+
+//   componentDidMount() {
+//     console.log("App Mounted");
+//     this.setState({ title: "hello lifecycle" });
+//   }
+
+//   handleClick = () => {
+//     this.setState({ isShowing: !this.state.isShowing });
+//   };
+
+//   render() {
+//     console.log("App Render");
+//     return (
+//       <section className="flex justify-center">
+//         <div className="w-1/2">
+//           <div className="text-center">
+//             <div className="my-4">{this.state.title}</div>
+//             <button
+//               className="p-1 my-2 bg-blue-700 text-white"
+//               onClick={this.handleClick}
+//             >
+//               Toggle Image
+//             </button>
+//           </div>
+//           {this.state.isShowing ? <Images /> : null}
+//         </div>
+//       </section>
+//     );
+//   }
+// }
