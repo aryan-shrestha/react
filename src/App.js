2f1c3d7286f870fd201ade6740a60d89ea7b0663
Aryan Shrestha <68954986+aryan-shrestha@users.noreply.github.com>
2022-04-04 12:20:43 +0545

    react router dom added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-// import React, { Component } from "react";
-
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Images from "./components/Images";
+import routes from "./utils/routes";
 import "./assets/css/style.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Gallery from "./pages/Gallery";
 
 function App() {
   return (
     <div className="app">
-      <Images />
+      {/* <Router>
+        <Switch>
+          {routes.map((route) => {
+            <Route
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />;
+          })}
+        </Switch>
+      </Router> */}
+      <Router>
+        <Switch>
+          {routes.map((route) => {
+            return (
+              <Route
+                path={route.path}
+                exact={route.exact}
+                component={route.component}
+              />
+            );
+          })}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Gallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Images from "../components/Images";
+
+function Gallery() {
+  return (
+    <div>
+      <Images />
+    </div>
+  );
+}
+
+export default Gallery;
