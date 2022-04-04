b7efcd61bc487332a6ea0a3b34cbfd4d6ae895ab
Aryan Shrestha <68954986+aryan-shrestha@users.noreply.github.com>
2022-04-04 19:09:16 +0545

    nav bar added

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import routes from "./utils/routes";
 import "./assets/css/style.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Gallery from "./pages/Gallery";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      {/* <Router>
-        <Switch>
-          {routes.map((route) => {
-            <Route
-              path={route.path}
-              exact={route.exact}
-              component={route.component}
-            />;
-          })}
-        </Switch>
-      </Router> */}
       <Router>
+        <Header />
         <Switch>
           {routes.map((route) => {
             return (
